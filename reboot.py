298f746b03adb291a12db95581c08c22ca7d9740
Coleman McFarland <coleman.mcfarland@gmail.com>
2012-10-28 18:26:09 -0400

    working reboot script

--- reboot.py
+++ reboot.py
@@ -1,7 +1,24 @@
 __author__ = 'coleman'
 
-import os
+# TODO make this more Pythonic, avoid using os.system()
 
+
+import os
+from subprocess import Popen
+from django.core.management import setup_environ, call_command
+from gbots import settings
+
+setup_environ(settings)
+
+print '\nHardcoded dumpdata command:'
+print "./manage.py dumpdata scraping dynamic_scraper --indent=2 > fixtures/starter.json\n"
+
+
+decision = raw_input('Overwrite fixture? yes/no: ')
+if decision == 'yes' or '':
+    os.system("./manage.py dumpdata scraping dynamic_scraper --indent=2 > fixtures/starter.json")
+else:
+    print 'Latest database not saved to fixture.\n'
 
 # trash old database and create new one
 os.system("rm db/gabybots.sqlite")
@@ -11,9 +28,15 @@ os.system("./manage.py syncdb --noinput")
 os.system("./manage.py migrate")
 
 # load the fixtures. This will have ALL our scrapers.
-print 'Loading the fixtures. These are our scrapers.'
+print '\nLoading the fixtures. These are our scrapers.'
 os.system("./manage.py loaddata ./fixtures/starter.json")
 
+
+
 # create superuser
-os.system("./manage.py createsuperuser --username=coleman --email=coleman.mcfarland@gmail.com")
+print "\n Creating superuser 'admin' with password 'testing123'"
+from django.contrib.auth.models import User
+User.objects.create_superuser(username='admin', email='admin@example.com', password='testing123')
+
+
 
