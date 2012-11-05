873220880680823815c1feeebf2a95b7a3bec1b1
Jeff May <viralidealogue@gmail.com>
2012-11-05 01:15:57 -0500

    source_spider [#5] - Added test_settings and runtests script.

---
+++ gbots/test_settings.py
@@ -0,0 +1,14 @@
+from gbots.settings import *
+
+DATABASES = {
+    'default': {
+        # postgresql_psycopg2, postgresql, mysql, sqlite3 or oracle
+        'ENGINE': 'django.db.backends.sqlite3',
+        # Path to database file (if using sqlite3)
+        'NAME': ':memory:', # Use an in-memory database
+        'USER': '', # Not used with sqlite3.
+        'PASSWORD': '', # Not used with sqlite3.
+        'HOST': '', # Set to empty string for localhost. Not used with sqlite3.
+        'PORT': '', # Set to empty string for default. Not used with sqlite3.
+    }
+}
