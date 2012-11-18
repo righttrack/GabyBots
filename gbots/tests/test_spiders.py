8e010fd77fdea20ca763ff0af9ab81148019bef9
Jeff May <viralidealogue@gmail.com>
2012-11-18 18:55:16 -0500

    fixed reboot script

--- gbots/tests/test_spiders.py
+++ gbots/tests/test_spiders.py
@@ -2,6 +2,9 @@ from dynamic_scraper.models import ScrapedObjClass
 from gbots.scraping.models import WebSource
 from gbots.tests import TestCase
 
+class FileSystemScrapingSpider(TestCase):
+    pass
+
 class GoogleNewsSourceSpiderTest(TestCase):
     fixtures = ['starter.json']  # TODO: make a testing fixture when this becomes too big
 
@@ -12,3 +15,4 @@ class GoogleNewsSourceSpiderTest(TestCase):
         source = WebSource.objects.get(alias="rss-google-world")
         article_obj_class = ScrapedObjClass.objects.get(name="Article")
         self.assertEqual(article_obj_class, source.scraper.scraped_obj_class)
+
--- reboot.py
+++ reboot.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python
+
 __author__ = 'coleman'
 
 # TODO make this more Pythonic, avoid using os.system()
@@ -15,13 +17,15 @@ from gbots.settings import PROJECT_ROOT
 
 setup_environ(settings)
 
+INITIAL_DATA = "gbots/scraping/fixtures/starter.json"
+
 print '\nPROJECT_ROOT variable:' + PROJECT_ROOT
-print "./manage.py dumpdata scraping dynamic_scraper --indent=2 > " + PROJECT_ROOT + "/gbots/scraping/starter.json\n"
+print "./manage.py dumpdata scraping dynamic_scraper --indent=2 > " + PROJECT_ROOT + "/" + INITIAL_DATA + "\n"
 
 
 decision = raw_input('Overwrite fixture? yes/no: ')
 if decision == 'yes' or '':
-    os.system(PROJECT_ROOT + "/manage.py dumpdata scraping dynamic_scraper --indent=2 > " + PROJECT_ROOT + "/fixtures/starter.json")
+    os.system(PROJECT_ROOT + "/manage.py dumpdata scraping dynamic_scraper --indent=2 > " + PROJECT_ROOT + "/" + INITIAL_DATA)
 else:
     print 'Latest database not saved to fixture.\n'
 
@@ -34,7 +38,7 @@ os.system(PROJECT_ROOT + "/manage.py migrate")
 
 # load the fixtures. This will have ALL our scrapers.
 print '\nLoading the fixtures. These are our scrapers.'
-os.system(PROJECT_ROOT + "/manage.py loaddata ./fixtures/starter.json")
+os.system(PROJECT_ROOT + "/manage.py loaddata " + PROJECT_ROOT + "/" + INITIAL_DATA)
 
 
 
