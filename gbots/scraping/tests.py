228f32d41b7225bb02df4e307e9e55c50b6a017b
Jeff May <viralidealogue@gmail.com>
2012-11-18 00:43:36 -0500

    source_spider [#5] - Cleaned up scraping tests.

--- gbots/scraping/tests.py
+++ gbots/scraping/tests.py
@@ -1,49 +1,50 @@
 """
-This file demonstrates writing tests using the unittest module. These will pass
-when you run "manage.py test".
-
-Replace this with more appropriate tests for your application.
+Tests for the scraping app.
 """
 
-from django.test import TestCase
+from gbots.tests import TestCase
 from gbots.scraping.processors import process
 
 
-class SubstituteProcessorTest(TestCase):
+class ProcessorTest(TestCase):
+    """
+    Tests process function.
+    """
+
     def test_parsing_empty_command(self):
         """
-        Tests that forward slashes are properly recognized
+        Forward slashes should be properly recognized.
         """
         self.assertEqual(process(r's///', 'same'), 'same')
 
     def test_parsing_simple_command(self):
         """
-        Tests that a simple substitution can be made
+        A simple substitution can be made.
         """
         self.assertEqual(process(r's/if/else/', 'if'), 'else')
 
     def test_parsing_inner_backslash(self):
         """
-        Tests that a escaped forward slashes inside the command will be parsed correctly
+        An escaped forward slashes inside the substitute command is parsed correctly.
         """
         self.assertEqual(process(r's/http:\/\/google.com/google/', 'http://google.com/home'),
             'google/home')
 
     def test_parsing_leading_backslash(self):
         """
-        Tests that a leading escaped forward slash in the find portion of the command will be parsed correctly
+        A leading escaped forward slash in the find portion of the substitute command is parsed correctly.
         """
         self.assertEqual(process(r's/\/img//', '/img/cool.jpg'), '/cool.jpg')
 
     def test_parsing_leading_and_trailing_backslash(self):
         """
-        Tests that leading and trailing escaped forward slashes can be parsed simultaneously in the find portion of the command
+        Leading and trailing escaped forward slashes in the find portion of the substitute command are parsed correctly.
         """
         self.assertEqual(process(r's/\/img//', '/img/cool.jpg'), '/cool.jpg')
 
     def test_escaped_slash_replacement(self):
         """
-        Tests that escaped forward slashes in the replace portion of the command will be parse correctly
+        Escaped forward slashes in the replace portion of the command are parsed correctly.
         """
         self.assertEqual(process(r's/\/img/http:\/\/sample.com\/img/', '/img/cool.jpg'),
             'http://sample.com/img/cool.jpg')
